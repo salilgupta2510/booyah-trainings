a25edfd18cb71be4a1706f7604cd9300f5c9c9f1
Vipul Malhotra <vipul.malhotra@centricconsulting.com>
2022-12-29 14:58:56 +0530

    updated icon

--- src/components/layout/partials/FooterSocial.js
+++ src/components/layout/partials/FooterSocial.js
@@ -34,7 +34,7 @@ const FooterSocial = ({
               height="24"
               viewBox="0 0 25 25"
               xmlns="http://www.w3.org/2000/svg">
-              <title>Twitter</title>
+              <title>Email</title>
               <path
                 d="M0 3v18h24v-18h-24zm21.518 2l-9.518 7.713-9.518-7.713h19.036zm-19.518 14v-11.817l10 8.104 10-8.104v11.817h-20z" />
             </svg>
@@ -107,7 +107,7 @@ const FooterSocial = ({
         </li>
       </ul>
       
-      <Overlay target={target.current} show={show} placement="top" style={{backgroundColor:'white'}}>
+      <Overlay target={target.current} show={show} placement="bottom" style={{backgroundColor:'white'}}>
         {(props) => (
           <Tooltip id="overlay-example" {...props}>
           +91 9810047018
